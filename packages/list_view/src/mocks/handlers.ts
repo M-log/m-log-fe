d3207ad421472f6bdacb9e8886301dfca2277381
TaejoonPark <--global>
2022-12-16 15:44:46 +0900

    :package: 패키지 추가 및 환경 설정
    - react
    - typescript
    - tailwindcss
    - msw

---
+++ packages/list_view/src/mocks/handlers.ts
@@ -0,0 +1,14 @@
+// import { rest } from "msw";
+
+export const handlers = [
+    // 할일 목록
+    // rest.get("/todos", (req, res, ctx) => {
+    //     return res(ctx.status(200), ctx.json(todos));
+    // }),
+
+    // 할일 추가
+    // rest.post("/todos", (req, res, ctx) => {
+    //     todos.push(req.body);
+    //     return res(ctx.status(201));
+    // })
+];
